f8ff84f81dba49c954efd5e27b8f0a3d83a41d34
zhushanbo <zhushanbo@baidu.com>
2021-06-09 19:50:58 +0800

    no message

--- PBSwift/其他/Main/AppDelegate.swift
+++ PBSwift/其他/Main/AppDelegate.swift
@@ -14,18 +14,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let window : UIWindow = UIWindow(frame: UIScreen.main.bounds)
+        let window: UIWindow = UIWindow(frame: UIScreen.main.bounds)
         self.window = window
         self.window!.makeKeyAndVisible()
         self.window!.backgroundColor = UIColor.white
         
         // tab
-        let tabController : PBTabBarController = PBTabBarController()
+        let tabController: PBTabBarController = PBTabBarController()
         
         // nav
-        let nav : UINavigationController = UINavigationController(rootViewController: tabController)
+        let nav: UINavigationController = UINavigationController(rootViewController: tabController)
         
-        // window!.rootViewController
+        // rootViewController
         self.window!.rootViewController = nav
         
         return true
--- PBSwift/其他/Main/PBTabBarController.swift
+++ PBSwift/其他/Main/PBTabBarController.swift
@@ -16,7 +16,7 @@ class PBTabBarController: UITabBarController {
         let attributesNormal = [NSAttributedStringKey.font : UIFont.systemFont(ofSize: 10), NSAttributedStringKey.foregroundColor : UIColor.gray]
         let attributesSelected = [NSAttributedStringKey.font : UIFont.systemFont(ofSize: 10), NSAttributedStringKey.foregroundColor : UIColor.red]
         
-        let vc1 : PBTestTwoController = PBTestTwoController()
+        let vc1: PBTestTwoController = PBTestTwoController()
         vc1.title = "首页"
         vc1.view.backgroundColor = UIColor(red: 244.0/255, green: 244.0/255, blue: 244.0/255, alpha: 1)
         vc1.tabBarItem.image = UIImage(named: "indexA")!.withRenderingMode(.alwaysOriginal)
@@ -26,8 +26,8 @@ class PBTabBarController: UITabBarController {
         vc1.tabBarItem.setTitleTextAttributes(attributesSelected, for: .selected)
         vc1.tabBarItem.titlePositionAdjustment = UIOffsetMake(0, -2)
         
-        let vc2 : UIViewController = UIViewController()
-        vc2.title = "广场"
+        let vc2: UIViewController = UIViewController()
+        vc2.title = "我的"
         vc2.view.backgroundColor = UIColor(red: 244.0/255, green: 244.0/255, blue: 244.0/255, alpha: 1)
         vc2.tabBarItem.image = UIImage(named: "squareA")!.withRenderingMode(.alwaysOriginal)
         vc2.tabBarItem.selectedImage = UIImage(named: "squareB")!.withRenderingMode(.alwaysOriginal)
@@ -36,26 +36,6 @@ class PBTabBarController: UITabBarController {
         vc2.tabBarItem.setTitleTextAttributes(attributesSelected, for: .selected)
         vc2.tabBarItem.titlePositionAdjustment = UIOffsetMake(0, -2)
         
-        let vc3 : UIViewController = UIViewController()
-        vc3.title = "活动"
-        vc3.view.backgroundColor = UIColor(red: 244.0/255, green: 244.0/255, blue: 244.0/255, alpha: 1)
-        vc3.tabBarItem.image = UIImage(named: "exerciseA")!.withRenderingMode(.alwaysOriginal)
-        vc3.tabBarItem.selectedImage = UIImage(named: "exerciseB")!.withRenderingMode(.alwaysOriginal)
-        vc3.tabBarItem.imageInsets = UIEdgeInsetsMake(-2, 0, 2, 0)
-        vc3.tabBarItem.setTitleTextAttributes(attributesNormal, for: UIControlState())
-        vc3.tabBarItem.setTitleTextAttributes(attributesSelected, for: .selected)
-        vc3.tabBarItem.titlePositionAdjustment = UIOffsetMake(0, -2)
-        
-        let vc4 : UIViewController = UIViewController()
-        vc4.title = "我的"
-        vc4.view.backgroundColor = UIColor(red: 244.0/255, green: 244.0/255, blue: 244.0/255, alpha: 1)
-        vc4.tabBarItem.image = UIImage(named: "mineA")!.withRenderingMode(.alwaysOriginal)
-        vc4.tabBarItem.selectedImage = UIImage(named: "mineB")!.withRenderingMode(.alwaysOriginal)
-        vc4.tabBarItem.imageInsets = UIEdgeInsetsMake(-2, 0, 2, 0)
-        vc4.tabBarItem.setTitleTextAttributes(attributesNormal, for: UIControlState())
-        vc4.tabBarItem.setTitleTextAttributes(attributesSelected, for: .selected)
-        vc4.tabBarItem.titlePositionAdjustment = UIOffsetMake(0, -2)
-        
-        self.viewControllers = [vc1, vc2, vc3, vc4];
+        self.viewControllers = [vc1, vc2]
     }
 }
