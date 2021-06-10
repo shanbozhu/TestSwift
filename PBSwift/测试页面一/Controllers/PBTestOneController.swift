3775bda17849a1637f3e53071a1d4470dad49e38
zhushanbo <zhushanbo@baidu.com>
2021-06-10 19:25:13 +0800

    no message

--- PBSwift/测试页面一/Controllers/PBTestOneController.swift
+++ PBSwift/测试页面一/Controllers/PBTestOneController.swift
@@ -18,30 +18,21 @@ class PBTestOneController: UIViewController, PBTestOneViewDelegate {
         print("\(sinceId)")
 
         let requestUrl = "https://m.baidu.com/recsys/hisproxy/data/usrhistory?page=0"
-//        var paras = [String : AnyObject]()
-//        paras["loginKey"] = "8546da3e-524f-4f49-86ac-5f3235e203ad" as AnyObject?
-//        paras["maitianId"] = "36"as AnyObject?
-//        paras["sinceId"] = "\(sinceId)"as AnyObject?
-//        paras["pageSize"] = "20"as AnyObject?
-//        paras["artistId"] = "0"as AnyObject?
-//
-//        var parasTwo = [String : AnyObject]()
-//        parasTwo["json"] = self.stringWithDictionary(paras) as AnyObject?
 
-        weak var weakSelf = self
+
+        
         let manager: AFHTTPRequestOperationManager = AFHTTPRequestOperationManager()
         manager.requestSerializer = AFHTTPRequestSerializer()
         manager.responseSerializer = AFHTTPResponseSerializer()
         
         let paras = self.requestHeaderAndBody(manager: manager)
         
-        
+        weak var weakSelf = self
         manager.post(requestUrl, parameters: paras, success: { (operation:AFHTTPRequestOperation?, responseObject:Any?) in
             let jsonDict : [String : AnyObject] = try! JSONSerialization.jsonObject(with: responseObject as! Data, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String : AnyObject]
-
+            
             let jsonStr = weakSelf!.stringWithDictionary(jsonDict)
             
-
             // 把字典封装成模型
             let testOne = PBTestOne.testOne(jsonDict)
 
@@ -68,6 +59,7 @@ class PBTestOneController: UIViewController, PBTestOneViewDelegate {
         }) { (operation :AFHTTPRequestOperation?, error:Error?) in
             print(error!)
         }
+        
         
         let jsonStr: String = "{\"data\" : {\"dynamic\" : [{\"content\" : \"最近两天貌似进入了蘑菇期，你们说会长又悄悄干嘛去了呢？[思考]大伙儿一起来聊聊呗。[阴险][偷乐] #李宇春#\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春\",\"sourceId\" : 288757,\"sortTime\" : 1460021145000,\"dynamicTime\" : 1460021145000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3961678180208127\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，晚安，好梦。你们说会长又悄悄干嘛去了呢？你们说会长又悄悄干嘛去了呢？你们说会长又悄悄干嘛去了呢？你们说会长又悄悄干嘛去了呢？你们说会长又悄悄干嘛去了呢？[思考]大伙儿一起来聊聊[思考]大伙儿一起来聊聊[思考]大伙儿一起来聊聊[思考]大伙儿一起来聊聊[思考]大伙儿一起来聊聊[思考]大伙儿一起来聊聊[思考]大伙儿一起来聊聊[思考]大伙儿一起来聊聊[思考]大伙儿一起来聊聊你们说会长又悄悄干嘛去了呢？[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春疯狂工作室\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"你们说会长又悄悄干嘛去了呢？你们说会长又悄悄干嘛去了呢？你们说会长又悄悄干嘛去了呢？你们说会长又悄悄干嘛去了呢？最近两天貌似进入了蘑菇期，你们说会长又悄悄干嘛去了呢？[思考]大伙儿一起来聊聊呗。[阴险][偷乐] #李宇春#\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春\",\"sourceId\" : 288757,\"sortTime\" : 1460021145000,\"dynamicTime\" : 1460021145000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3961678180208127\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"最近两天貌似进入了蘑菇期，你们说会长又悄悄干嘛去了呢？[思考]大伙儿一起来聊聊呗。你们说会长又悄悄干嘛去了呢？你们说会长又悄悄干嘛去了呢？[阴险][偷乐] #李宇春#\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春疯狂工作室\",\"sourceId\" : 288757,\"sortTime\" : 1460021145000,\"dynamicTime\" : 1460021145000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3961678180208127\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"最近两天貌似进入了蘑菇期，你们似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又说会长又悄悄干嘛去了呢？[思考]大伙儿一起来聊聊呗。[阴险][偷乐] #李宇春#\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春疯狂工作室\",\"sourceId\" : 288757,\"sortTime\" : 1460021145000,\"dynamicTime\" : 1460021145000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3961678180208127\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春疯狂工作室\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春疯狂工作室\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又爱你，么么哒，晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春疯狂工作室\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春疯狂工作室\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6}],\"artistList\" : [{\"artistId\" : 67,\"name\" : \"李宇春\"},{\"artistId\" : 197,\"name\" : \"李宇春疯狂工作室\"}]},\"code\" : 200}"
         
