2be9dd8d0d632e13383ff7bea22e259411bfc762
zhushanbo <zhushanbo@baidu.com>
2021-06-10 16:41:50 +0800

    no message

--- PBSwift/测试页面一/Models/PBTestOneDataDynamic.swift
+++ PBSwift/测试页面一/Models/PBTestOneDataDynamic.swift
@@ -26,7 +26,7 @@ class PBTestOneDataDynamic: NSObject {
         // 所有键全部赋值
         self.setValuesForKeys(dict)
         
-        img2 = ["http://imgsrc.baidu.com/baike/pic/item/3bf33a87e950352a344d78d55643fbf2b2118b3b.jpg"]
+        self.img2 = ["http://imgsrc.baidu.com/baike/pic/item/3bf33a87e950352a344d78d55643fbf2b2118b3b.jpg"]
     }
     
     override func setValue(_ value: Any?, forUndefinedKey key: String) {
