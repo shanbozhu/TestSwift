d3ea177f06b09c66f0993de54cf452e2b4407f34
zhushanbo <zhushanbo@baidu.com>
2021-06-10 18:51:45 +0800

    no message

--- PBSwift/测试页面一/Controllers/PBTestOneController.swift
+++ PBSwift/测试页面一/Controllers/PBTestOneController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-let sWidth : CGFloat = UIScreen.main.bounds.size.width;
-let sHeight : CGFloat = UIScreen.main.bounds.size.height;
+let sWidth: CGFloat = UIScreen.main.bounds.size.width
+let sHeight: CGFloat = UIScreen.main.bounds.size.height
 
 class PBTestOneController: UIViewController, PBTestOneViewDelegate {
     weak var testOneView: PBTestOneView?
@@ -19,17 +19,17 @@ class PBTestOneController: UIViewController, PBTestOneViewDelegate {
 
         let requestUrl = "http://mtapi.damai.cn/mt/social/weiBoList"
         var paras = [String : AnyObject]()
-        paras["loginKey"] = "8546da3e-524f-4f49-86ac-5f3235e203ad" as AnyObject?;
-        paras["maitianId"] = "36"as AnyObject?;
-        paras["sinceId"] = "\(sinceId)"as AnyObject?;
-        paras["pageSize"] = "20"as AnyObject?;
-        paras["artistId"] = "0"as AnyObject?;
+        paras["loginKey"] = "8546da3e-524f-4f49-86ac-5f3235e203ad" as AnyObject?
+        paras["maitianId"] = "36"as AnyObject?
+        paras["sinceId"] = "\(sinceId)"as AnyObject?
+        paras["pageSize"] = "20"as AnyObject?
+        paras["artistId"] = "0"as AnyObject?
 
         var parasTwo = [String : AnyObject]()
         parasTwo["json"] = self.stringWithDictionary(paras) as AnyObject?
 
         weak var weakSelf = self
-        let manager : AFHTTPRequestOperationManager = AFHTTPRequestOperationManager()
+        let manager: AFHTTPRequestOperationManager = AFHTTPRequestOperationManager()
         manager.responseSerializer = AFHTTPResponseSerializer()
         manager.post(requestUrl, parameters: parasTwo, success: { (operation:AFHTTPRequestOperation?, responseObject:Any?) in
             let jsonDict : [String : AnyObject] = try! JSONSerialization.jsonObject(with: responseObject as! Data, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String : AnyObject]
@@ -66,7 +66,7 @@ class PBTestOneController: UIViewController, PBTestOneViewDelegate {
         
         let jsonStr: String = "{\"data\" : {\"dynamic\" : [{\"content\" : \"最近两天貌似进入了蘑菇期，你们说会长又悄悄干嘛去了呢？[思考]大伙儿一起来聊聊呗。[阴险][偷乐] #李宇春#\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春\",\"sourceId\" : 288757,\"sortTime\" : 1460021145000,\"dynamicTime\" : 1460021145000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3961678180208127\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，晚安，好梦。你们说会长又悄悄干嘛去了呢？你们说会长又悄悄干嘛去了呢？你们说会长又悄悄干嘛去了呢？你们说会长又悄悄干嘛去了呢？你们说会长又悄悄干嘛去了呢？[思考]大伙儿一起来聊聊[思考]大伙儿一起来聊聊[思考]大伙儿一起来聊聊[思考]大伙儿一起来聊聊[思考]大伙儿一起来聊聊[思考]大伙儿一起来聊聊[思考]大伙儿一起来聊聊[思考]大伙儿一起来聊聊[思考]大伙儿一起来聊聊你们说会长又悄悄干嘛去了呢？[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春疯狂工作室\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"你们说会长又悄悄干嘛去了呢？你们说会长又悄悄干嘛去了呢？你们说会长又悄悄干嘛去了呢？你们说会长又悄悄干嘛去了呢？最近两天貌似进入了蘑菇期，你们说会长又悄悄干嘛去了呢？[思考]大伙儿一起来聊聊呗。[阴险][偷乐] #李宇春#\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春\",\"sourceId\" : 288757,\"sortTime\" : 1460021145000,\"dynamicTime\" : 1460021145000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3961678180208127\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"最近两天貌似进入了蘑菇期，你们说会长又悄悄干嘛去了呢？[思考]大伙儿一起来聊聊呗。你们说会长又悄悄干嘛去了呢？你们说会长又悄悄干嘛去了呢？[阴险][偷乐] #李宇春#\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春疯狂工作室\",\"sourceId\" : 288757,\"sortTime\" : 1460021145000,\"dynamicTime\" : 1460021145000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3961678180208127\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"最近两天貌似进入了蘑菇期，你们似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又说会长又悄悄干嘛去了呢？[思考]大伙儿一起来聊聊呗。[阴险][偷乐] #李宇春#\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春疯狂工作室\",\"sourceId\" : 288757,\"sortTime\" : 1460021145000,\"dynamicTime\" : 1460021145000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3961678180208127\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春疯狂工作室\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春疯狂工作室\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又爱你，么么哒，晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春疯狂工作室\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6},{\"content\" : \"爱你，么么哒，似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又似进入了蘑菇期，你们说会长又晚安，好梦。[心]\",\"isSupport\" : 0,\"type\" : 4,\"title\" : \"李宇春疯狂工作室\",\"sourceId\" : 286715,\"sortTime\" : 1459695876000,\"dynamicTime\" : 1459695876000,\"favorCount\" : 0,\"socialType\" : 4,\"statusId\" : \"3960313902310751\",\"artId\" : \"2723838314\",\"img2\" : [],\"replyCount\" : 0,\"maitianId\" : 6}],\"artistList\" : [{\"artistId\" : 67,\"name\" : \"李宇春\"},{\"artistId\" : 197,\"name\" : \"李宇春疯狂工作室\"}]},\"code\" : 200}"
         
-        let data: Data = jsonStr.data(using: String.Encoding(rawValue: 4))!
+        let data: Data = jsonStr.data(using: String.Encoding.utf8)!
         let jsonDict: [String : AnyObject] = try! JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String : AnyObject]
         
         // 2.把字典封装成模型
@@ -95,7 +95,7 @@ class PBTestOneController: UIViewController, PBTestOneViewDelegate {
     func testOneView(_ testOneView: PBTestOneView) {
         print("我被点击了")
         
-        let vc : UIViewController = UIViewController()
+        let vc: UIViewController = UIViewController()
         vc.hidesBottomBarWhenPushed = true
         vc.view.backgroundColor = UIColor.white
         
@@ -113,15 +113,15 @@ class PBTestOneController: UIViewController, PBTestOneViewDelegate {
     }
     
     func stringWithDictionary(_ jsonDict : [String : AnyObject]) -> String {
-        let data : Data = try! JSONSerialization.data(withJSONObject: jsonDict, options: JSONSerialization.WritingOptions.prettyPrinted)
+        let data: Data = try! JSONSerialization.data(withJSONObject: jsonDict, options: JSONSerialization.WritingOptions.prettyPrinted)
         
         return String(data:data, encoding:String.Encoding.utf8)!
     }
     
     func dictionaryWithString(_ jsonStr : String) -> [String : AnyObject] {
-        let data : Data = jsonStr.data(using: String.Encoding.utf8)!
+        let data: Data = jsonStr.data(using: String.Encoding.utf8)!
         
-        let jsonDict : [String : AnyObject] = try! JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String : AnyObject]
+        let jsonDict: [String : AnyObject] = try! JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String : AnyObject]
         return jsonDict
     }
 }
